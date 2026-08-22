09719ed6a28a7165587cb1943f935021dbd6effa
financepbr <antdnz01@gmail.com>
2026-08-23 01:51:48 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
    (v1 -> v2 -> v3 ...). Kalau tidak dinaikkan, alat petugas akan
    tetap memakai versi lama yang tersimpan.
    ============================================================ */
-var CACHE = 'so-plataran-v11';
+var CACHE = 'so-plataran-v13';
 var ASSETS = ['./', './index.html', './manifest.json'];
 
 /* Saat pertama dipasang: simpan file app ke cache */
